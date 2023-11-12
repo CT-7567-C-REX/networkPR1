64df46c70c01fee0aa31b4458eacff412e9cda4e
Salman Tas <salman.tas34@gmail.com>
2023-11-12 06:14:07 +0300

    final version

--- app.py
+++ app.py
@@ -1,46 +1,74 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField ,SubmitField
+from wtforms.validators import DataRequired
 from flask_restful import Api, Resource
 
+class PostForm(FlaskForm):
+    index = IntegerField('Enter the index', validators=[DataRequired()])
+    addable = StringField('Enter something', validators=[DataRequired()])
+    submit = SubmitField('add to spesific location')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "this is a key dude"
 api = Api(app)
 
 mylist = []
 
 class This_is_delete_methode(Resource):
     def delete(self, id):
-        id = int(id)  # Convert id to integer
+        
         if id >= len(mylist):
             return "Index out of range", 400
         else:
             del mylist[id]
-            return
+            return 
         
 class This_is_patch_methode(Resource):
     def patch(self, id, newval):
         id = int(id)  # Convert id to integer
         if id >= len(mylist):
-            return '', 401
+            return 'Index out of range', 400
         else:
             mylist[id] = newval
             return
 
 class This_is_put_methode(Resource):
     def put(self, addable):
+        if addable in mylist:
+            return "Resource already exists", 409
         mylist.append(addable)
-        return "array updated", 201
+        return "Array updated", 201
+    
+class This_is_get_methode(Resource):
+    def get(self):
+        if not mylist:
+            return '', 204  # Return 204 No Content if the list is empty
+        return mylist, 200
 
 api.add_resource(This_is_delete_methode, "/deletelement/<int:id>")
 api.add_resource(This_is_put_methode, "/putelement/<string:addable>")
 api.add_resource(This_is_patch_methode, "/patchelement/<int:id>/<string:newval>")
-
-
+api.add_resource(This_is_get_methode, "/mylist")
+ 
 
 @app.route("/")
 def home():
+    form = PostForm()
+    return render_template("index.html", entity = mylist, addr = url_for('home', _external = True), form = form)
 
-    return render_template("index.html", entity = mylist, addr = url_for('home', _external = True))
+
+@app.route("/acceptpost", methods=['POST'])
+def posthandler():
+    form = PostForm(request.form)
+    if form.validate_on_submit():
+        a = form.index.data
+        b = form.addable.data
+        if a >= len(mylist):
+            return
+        else:
+            mylist.insert(a, b)
+        return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
--- static/scriptjava/script.js
+++ static/scriptjava/script.js
@@ -1,5 +1,4 @@
 
-
 // put method triger
 const btnEL = document.querySelector('.addButton');
 const inputEL = document.querySelector('.textInput');
@@ -11,13 +10,8 @@ const clickHandler = () => {
         method: 'PUT',
     })
     .then((response) => {
-        if (response.status === 200) {
-            statusEL.textContent = 'Resource found (200)';
-            inputEL.value = ''
-        } 
-        else{
-            statusEL.textContent = response.status;
-        }
+        inputEL.value = ''
+        statusEL.textContent = response.status;
     })
 };
 btnEL.addEventListener('click', clickHandler);
@@ -34,15 +28,64 @@ const delclickHandler = () => {
         method: 'DELETE',
     })
     .then((response) => {
-        if (response.status === 200) {
-            delstatusEL.textContent = 'Resource found (200)';
-            inputEL.value = ''
-        } 
-        else{
-            delstatusEL.textContent = response.status;
-        }
+        delinputEL.value = ''
+        delstatusEL.textContent = response.status;
+    
     })
 };
 delbtnEL.addEventListener('click', delclickHandler);
 //end of delete methode triger
 
+//patch methode triger
+const patchbtnEL = document.querySelector('.patchteButton');  // Corrected class name
+const patchinputEL = document.querySelector('.textInputpatch');  // Corrected class name
+const patchinputEL2 = document.querySelector('.textInputpatchone');  // Corrected class name
+const patchstatusEL = document.querySelector('.statusthree');  // Corrected class name
+
+const patchclickHandler = () => {
+    const patchinputValue = patchinputEL.value;
+    const patchinputValue2 = patchinputEL2.value;
+    fetch(`${ipAddress}/patchelement/${encodeURIComponent(patchinputValue2)}/${encodeURIComponent(patchinputValue)}`, {
+        method: 'PATCH', 
+    })
+    .then((response) => {
+            patchinputEL.value = ''
+            patchinputEL2.value = ''
+            patchstatusEL.textContent = response.status;  // Corrected variable name
+    })
+};
+
+patchbtnEL.addEventListener('click', patchclickHandler);  // Corrected class name
+
+//patch of delete methode triger
+
+
+
+//get methode triger
+const getbtnEL = document.querySelector('.getbtn');
+const getstatusEL = document.querySelector('.statusget');
+const listUL = document.querySelector('ul'); // Get the <ul> element
+
+const getclickHandler = () => {
+    fetch(`${ipAddress}/mylist`, {})
+    .then((response) => {
+        getstatusEL.textContent = response.status;
+        if (response.status === 200) {
+            return response.json(); // Parse the response as JSON
+        } else {
+            return []; // Return an empty array if the response status is not 200
+        }
+    })
+    .then((data) => {
+        listUL.innerHTML = ''; // Clear the existing list items
+        data.forEach((item) => {
+            const listItem = document.createElement('li');
+            listItem.textContent = item;
+            listUL.appendChild(listItem); // Append the list item to the <ul>
+        });
+    });
+};
+
+getbtnEL.addEventListener('click', getclickHandler);
+
+//end of get methode triger
